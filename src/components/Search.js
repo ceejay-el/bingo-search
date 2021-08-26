4d588cdf503fc9e269085efd2e319b74a2502017
ceejay-el <59852540+ceejay-el@users.noreply.github.com>
2021-08-25 17:17:53 -0700

    input text can now be used to query

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 import search_icon from "../images/search-icon.svg";
 import "./Components.css";
 
 export default function Search(){
+    // change state with user input inside search bar
+    // input text is empty by default
+    const [text, setText] = useState("");
+
+    /**
+     * TODO: serve data to the user
+     */
+    function query(event){
+        event.preventDefault();
+    }
+
+    // input tag captures text and changes state
     return (
         <div className="search">
             <form action="/" method="get" className="inputform">
@@ -12,8 +24,10 @@ export default function Search(){
                     id="searchbar"
                     placeholder="Search"
                     className="inputbox"
+                    value={text}
+                    onChange={event => {setText(event.target.value)}}
                 />
-                <button type="submit" className="searchbtn">
+                <button type="submit" className="searchbtn" onClick={query}>
                     <img src={search_icon} alt="Search button"/>
                 </button>
             </form>
