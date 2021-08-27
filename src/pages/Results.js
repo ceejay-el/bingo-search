7b92bc7dd3287beb968038569b546a633dd514c7
ceejay-el <59852540+ceejay-el@users.noreply.github.com>
2021-08-27 15:43:54 -0700

    oops. I meant to remove that

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,12 +1,10 @@
 import React from "react";
-import useSearchEngine from "../components/useSearchEngine.js";
 
 export default function Results(){
-    const {data} = useSearchEngine(term);
 
     return (
         <div>
-            {data}
+            data
         </div>
     );
 }
