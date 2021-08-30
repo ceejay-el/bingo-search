16b8a513f514f0a14d67227011ddb5ed4577b05e
ceejay-el <59852540+ceejay-el@users.noreply.github.com>
2021-08-30 14:31:30 -0700

    use style as jsx, not html tag

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Results(){
 
     return (
-        <div style="margin: 20%">
+        <div style={{margin: "20%"}}>
             I'm working on it. The site will be live in a few, I just gotta figure something out.
         </div>
     );
